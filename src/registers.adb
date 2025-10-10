71e922d0df0910931cd9ace35ef648c0bcca5a21
moehr1z <mooehr1z@gmail.com>
2025-10-10 15:32:48 +0200

    Add Registers

---
+++ src/registers.adb
@@ -0,0 +1,50 @@
+package body Registers is
+
+   procedure Set_General_Register
+     (Number : General_Register_Number; Value : Word) is
+   begin
+      General_Registers (Number) := Value;
+   end Set_General_Register;
+
+   function Get_General_Register (Number : General_Register_Number) return Word
+   is (General_Registers (Number));
+
+   procedure Set_Address_Register (Value : Address) is
+   begin
+      Address_Register := Value;
+   end Set_Address_Register;
+
+   function Get_Address_Register return Address
+   is (Address_Register);
+
+   function Carry_Is_Set return Boolean
+   is (Get_General_Register (VF) /= 0);
+
+   procedure Set_Carry (State : Boolean) is
+   begin
+      Set_VF_From_Boolean (State);
+   end Set_Carry;
+
+   function No_Borrow_Is_Set return Boolean
+   is (Get_General_Register (VF) /= 0);
+   procedure Set_No_Borrow (State : Boolean) is
+   begin
+      Set_VF_From_Boolean (State);
+   end Set_No_Borrow;
+
+   function Collision_Is_Set return Boolean
+   is (Get_General_Register (VF) /= 0);
+   procedure Set_Collision (State : Boolean) is
+   begin
+      Set_VF_From_Boolean (State);
+   end Set_Collision;
+
+   procedure Set_VF_From_Boolean (B : Boolean) is
+      Value : Word := 0;
+   begin
+      if B then
+         Value := 1;
+      end if;
+      Set_General_Register (VF, Value);
+   end Set_VF_From_Boolean;
+end Registers;
---
+++ src/registers.ads
@@ -0,0 +1,50 @@
+with Machine; use Machine;
+
+package Registers is
+   type General_Register_Number is range 0 .. 16#F#;
+
+   procedure Set_General_Register
+     (Number : General_Register_Number; Value : Word)
+     -- Postcondition: The specified register is really set to Value and no other is modified
+   with Post => Get_General_Register (Number) = Value;
+   --  TODO: how to properly express this?
+   --  and then (for all I in General_Register_Number =>
+   --              I = Number
+   --              or else (Get_General_Register (I)
+   --                       = Get_General_Register (I)'Old
+   --                       and Get_Address_Register
+   --                           = Get_Address_Register'Old));
+
+   function Get_General_Register
+     (Number : General_Register_Number) return Word;
+
+   procedure Set_Address_Register (Value : Address)
+   with Post => Get_Address_Register = Value;
+
+   function Get_Address_Register return Address;
+
+   function Carry_Is_Set return Boolean;
+
+   procedure Set_Carry (State : Boolean)
+   with Post => Carry_Is_Set = State;
+
+   function No_Borrow_Is_Set return Boolean;
+
+   procedure Set_No_Borrow (State : Boolean)
+   with Post => No_Borrow_Is_Set = State;
+
+   function Collision_Is_Set return Boolean;
+
+   procedure Set_Collision (State : Boolean)
+   with Post => Collision_Is_Set = State;
+
+private
+   General_Registers : array (General_Register_Number) of Word :=
+     (others => 0);
+   Address_Register  : Address := 0;
+
+   VF : constant General_Register_Number := 16#F#;
+
+   procedure Set_VF_From_Boolean (B : Boolean)
+   with Post => Get_General_Register (VF) = (if B then 1 else 0);
+end Registers;
