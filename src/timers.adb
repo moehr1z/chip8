ffe6431ac49d372434974015d9d70524cb9163b1
moehr1z <mooehr1z@gmail.com>
2025-10-11 11:15:44 +0200

    Timers: Fix semantics + add setters

--- src/timers.adb
+++ src/timers.adb
@@ -1,12 +1,36 @@
 package body Timers is
    procedure Update_Timers is
    begin
-      Delay_Timer_Value := Delay_Timer_Value - 1;
-      Sound_Timer_Value := Sound_Timer_Value - 1;
+      Update_Delay_Timer;
+      Update_Sound_Timer;
    end Update_Timers;
 
-   function Get_Delay_Timer return Delay_Timer
+   procedure Update_Delay_Timer is
+   begin
+      if Get_Delay_Timer > 0 then
+         Delay_Timer_Value := Delay_Timer_Value - 1;
+      end if;
+   end Update_Delay_Timer;
+
+   procedure Update_Sound_Timer is
+   begin
+      if Get_Sound_Timer > 0 then
+         Sound_Timer_Value := Sound_Timer_Value - 1;
+      end if;
+   end Update_Sound_Timer;
+
+   procedure Set_Delay_Timer (Value : Timer) is
+   begin
+      Delay_Timer_Value := Value;
+   end Set_Delay_Timer;
+
+   procedure Set_Sound_Timer (Value : Timer) is
+   begin
+      Sound_Timer_Value := Value;
+   end Set_Sound_Timer;
+
+   function Get_Delay_Timer return Timer
    is (Delay_Timer_Value);
-   function Get_Sound_Timer return Sound_Timer
+   function Get_Sound_Timer return Timer
    is (Sound_Timer_Value);
 end Timers;
--- src/timers.ads
+++ src/timers.ads
@@ -1,18 +1,35 @@
 package Timers is
-
    Rate : constant Positive := 60;
-   type Delay_Timer is mod Rate;
-   type Sound_Timer is mod Rate;
 
-   procedure Update_Timers
+   type Timer is range 0 .. 2**8 - 1;
+
+   procedure Update_Timers;
+
+   procedure Set_Delay_Timer (Value : Timer)
+   with Post => Get_Delay_Timer = Value;
+
+   procedure Set_Sound_Timer (Value : Timer)
+   with Post => Get_Sound_Timer = Value;
+
+   function Get_Delay_Timer return Timer;
+   function Get_Sound_Timer return Timer;
+private
+   Delay_Timer_Value : Timer := Timer'Last;
+   Sound_Timer_Value : Timer := Timer'Last;
+
+   procedure Update_Delay_Timer
    with
      Post =>
-       Get_Delay_Timer /= Get_Delay_Timer'Old
-       and Get_Sound_Timer /= Get_Sound_Timer'Old;
+       (if Delay_Timer_Value'Old = 0
+        then
+          Delay_Timer_Value = Delay_Timer_Value'Old
+          or else Delay_Timer_Value < Delay_Timer_Value'Old);
 
-   function Get_Delay_Timer return Delay_Timer;
-   function Get_Sound_Timer return Sound_Timer;
-private
-   Delay_Timer_Value : Delay_Timer := Delay_Timer'Last;
-   Sound_Timer_Value : Sound_Timer := Sound_Timer'Last;
+   procedure Update_Sound_Timer
+   with
+     Post =>
+       (if Sound_Timer_Value'Old = 0
+        then
+          Sound_Timer_Value = Sound_Timer_Value'Old
+          or else Sound_Timer_Value < Sound_Timer_Value'Old);
 end Timers;
