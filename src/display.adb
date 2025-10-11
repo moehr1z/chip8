5085e4191767f7f7f2f784f5db1a9bfd8f0bd898
moehr1z <mooehr1z@gmail.com>
2025-10-11 11:16:32 +0200

    Add Display

---
+++ src/display.adb
@@ -0,0 +1,12 @@
+package body Display is
+
+   procedure Init is
+   begin
+      null;
+   end Init;
+   procedure Draw is
+   begin
+      null;
+   end Draw;
+
+end Display;
---
+++ src/display.ads
@@ -0,0 +1,8 @@
+package Display is
+   Width  : constant Integer := 64;
+   Height : constant Integer := 32;
+
+   procedure Init;
+   procedure Draw;
+
+end Display;
