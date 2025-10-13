e8d1b765d31848bf7e0dd1fd90ed45181b22325a
moehr1z <mooehr1z@gmail.com>
2025-10-13 11:14:13 +0200

    Memory: Rename Address to User_Address

--- src/memory.adb
+++ src/memory.adb
@@ -1,21 +1,23 @@
 with Ada.Sequential_IO;
 
 package body Memory is
-   function Load (A : Address) return Memory_Word
+   function Load (A : User_Address) return Memory_Word
    is (Data_Space (A));
 
-   procedure Store (A : Address; W : Memory_Word) is
+   procedure Store (A : User_Address; W : Memory_Word) is
    begin
       Data_Space (A) := W;
    end Store;
 
    procedure Load_Program (File_Name : String) is
-      Loader_Index : Address := Data_Space'First;
+      Loader_Index : User_Address := Data_Space'First;
       package Word_IO is new Ada.Sequential_IO (Memory_Word);
       use Word_IO;
       F            : Word_IO.File_Type;
       Current_Word : Memory_Word := 0;
    begin
+      -- TODO: error handling (exceptions, file bigger than memory etc)
+
       Open (F, In_File, File_Name);
       while not End_Of_File (F) loop
          Read (F, Current_Word);
--- src/memory.ads
+++ src/memory.ads
@@ -1,13 +1,13 @@
 package Memory is
    type Memory_Word is mod 2**8;
-   type Address is
+   type User_Address is
      range 16#200# .. 16#FFF#; -- there's actually 12 bits for addresses,
    --  but from 0x000 to 0x1FF there was the location of the original interpreter.
    --  So programs should only use the range given above.
 
-   function Load (A : Address) return Memory_Word;
-   procedure Store (A : Address; W : Memory_Word);
+   function Load (A : User_Address) return Memory_Word;
+   procedure Store (A : User_Address; W : Memory_Word);
    procedure Load_Program (File_Name : String);
 private
-   Data_Space : array (Address) of Memory_Word := (others => 0);
+   Data_Space : array (User_Address) of Memory_Word := (others => 0);
 end Memory;
