69c0c0c2b69db79ee1f752a04a6c21f67a340c00
moehr1z <mooehr1z@gmail.com>
2025-10-14 12:21:59 +0200

    Instructions: Implement all instruction handlers

--- src/instructions.adb
+++ src/instructions.adb
@@ -1,9 +1,18 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Conversions;
+with Display;
+with Sprite;
+with Stack;
+with Timers;
+with Keypad;
+with Random_Numbers;
+
 package body Instructions is
    procedure Step is
       O : Opcode;
    begin
       Fetch (O);
-      Decode_And_Execute (O);
+      Execute (O);
    end Step;
 
    procedure Fetch (O : out Opcode) is
@@ -12,154 +21,307 @@ package body Instructions is
 
    end Fetch;
 
-   procedure Decode_And_Execute (O : Opcode) is
+   procedure Execute (O : Opcode) is
    begin
       null;
 
-   end Decode_And_Execute;
+   end Execute;
 
    -- Instruction handlers
    --
    --
    procedure Handle_Cls is
    begin
-      null;
+      Display.Clear;
    end Handle_Cls;
+
    procedure Handle_Ret is
+      Return_Address : User_Address;
    begin
-      null;
+      Stack.Pop (Return_Address);
+      Set_Program_Counter (Return_Address);
    end Handle_Ret;
-   procedure Handle_Sys_Addr (Addr : Address) is
+
+   procedure Handle_Sys_Addr (Target_Address : User_Address) is
    begin
-      null;
+      Put_Line
+        ("The SYS  instruction is only used on the old computers on which Chip-8 was originally implemented. Ignoring it. (Target_Address = "
+         & Target_Address'Image
+         & ")");
    end Handle_Sys_Addr;
-   procedure Handle_Jp_Addr (Addr : Address) is
+
+   procedure Handle_Jp_Addr (Target_Address : User_Address) is
    begin
-      null;
+      Set_Program_Counter (Target_Address);
    end Handle_Jp_Addr;
-   procedure Handle_Call_Addr (Addr : Address) is
+
+   procedure Handle_Call_Addr (Target_Address : User_Address) is
    begin
-      null;
+      Stack.Push (Get_Program_Counter);
+      Set_Program_Counter (Target_Address);
    end Handle_Call_Addr;
-   procedure Handle_Jp_V0_Addr (Addr : Address) is
+
+   -- TODO: check if it fits
+   procedure Handle_Jp_V0_Addr (Target_Address : User_Address) is
+      Value         : constant Register_Word := Get_General_Register (0);
+      Final_Address : constant User_Address :=
+        User_Address (Positive (Target_Address) + Positive (Value));
    begin
-      null;
+      Set_Program_Counter (Final_Address);
    end Handle_Jp_V0_Addr;
-   procedure Handle_Ld_I_Addr (Addr : Address) is
+
+   procedure Handle_Ld_I_Addr (Target_Address : Address) is
    begin
-      null;
+      Set_Address_Register (Target_Address);
    end Handle_Ld_I_Addr;
-   procedure Handle_Se_Vx_Byte (Vx : Register_Word; B : Byte) is
+
+   procedure Handle_Se_Vx_Byte (Register_1 : General_Register_Number; B : Byte)
+   is
+      Value : constant Register_Word := Get_General_Register (Register_1);
    begin
-      null;
+      if Byte (Value) = B then
+         Increment_Program_Counter;
+      end if;
    end Handle_Se_Vx_Byte;
-   procedure Handle_Sne_Vx_Byte (Vx : Register_Word; B : Byte) is
+
+   procedure Handle_Sne_Vx_Byte
+     (Register_1 : General_Register_Number; B : Byte)
+   is
+      Value : constant Register_Word := Get_General_Register (Register_1);
    begin
-      null;
+      if Byte (Value) /= B then
+         Increment_Program_Counter;
+      end if;
    end Handle_Sne_Vx_Byte;
-   procedure Handle_Ld_Vx_Byte (Vx : Register_Word; B : Byte) is
-   begin
-      null;
-   end Handle_Ld_Vx_Byte;
-   procedure Handle_Add_Vx_Byte (Vx : Register_Word; B : Byte) is
-   begin
-      null;
-   end Handle_Add_Vx_Byte;
-   procedure Handle_Rnd_Vx_Byte (Vx : Register_Word; B : Byte) is
-   begin
-      null;
-   end Handle_Rnd_Vx_Byte;
-   procedure Handle_Skp_Vx (Vx : Register_Word) is
-   begin
-      null;
-   end Handle_Skp_Vx;
-   procedure Handle_Sknp_Vx (Vx : Register_Word) is
-   begin
-      null;
-   end Handle_Sknp_Vx;
-   procedure Handle_Ld_Vx_Dt (Vx : Register_Word) is
-   begin
-      null;
-   end Handle_Ld_Vx_Dt;
-   procedure Handle_Ld_Vx_K (Vx : Register_Word) is
-   begin
-      null;
-   end Handle_Ld_Vx_K;
-   procedure Handle_Ld_Dt_Vx (Vx : Register_Word) is
-   begin
-      null;
-   end Handle_Ld_Dt_Vx;
-   procedure Handle_Ld_St_Vx (Vx : Register_Word) is
-   begin
-      null;
-   end Handle_Ld_St_Vx;
-   procedure Handle_Add_I_Vx (Vx : Register_Word) is
-   begin
-      null;
-   end Handle_Add_I_Vx;
-   procedure Handle_Ld_F_Vx (Vx : Register_Word) is
-   begin
-      null;
-   end Handle_Ld_F_Vx;
-   procedure Handle_Ld_B_Vx (Vx : Register_Word) is
-   begin
-      null;
-   end Handle_Ld_B_Vx;
-   procedure Handle_Ld_I_Vx (Vx : Register_Word) is
-   begin
-      null;
-   end Handle_Ld_I_Vx;
-   procedure Handle_Ld_Vx_I (Vx : Register_Word) is
-   begin
-      null;
-   end Handle_Ld_Vx_I;
-   procedure Handle_Se_Vx_Vy (Vx : Register_Word; Vy : Register_Word) is
-   begin
-      null;
-   end Handle_Se_Vx_Vy;
-   procedure Handle_Ld_Vx_Vy (Vx : Register_Word; Vy : Register_Word) is
-   begin
-      null;
-   end Handle_Ld_Vx_Vy;
-   procedure Handle_Or_Vx_Vy (Vx : Register_Word; Vy : Register_Word) is
-   begin
-      null;
-   end Handle_Or_Vx_Vy;
-   procedure Handle_And_Vx_Vy (Vx : Register_Word; Vy : Register_Word) is
-   begin
-      null;
-   end Handle_And_Vx_Vy;
-   procedure Handle_Xor_Vx_Vy (Vx : Register_Word; Vy : Register_Word) is
-   begin
-      null;
-   end Handle_Xor_Vx_Vy;
-   procedure Handle_Add_Vx_Vy (Vx : Register_Word; Vy : Register_Word) is
-   begin
-      null;
-   end Handle_Add_Vx_Vy;
-   procedure Handle_Sub_Vx_Vy (Vx : Register_Word; Vy : Register_Word) is
-   begin
-      null;
-   end Handle_Sub_Vx_Vy;
-   procedure Handle_Shr_Vx_Vy (Vx : Register_Word; Vy : Register_Word) is
-   begin
-      null;
-   end Handle_Shr_Vx_Vy;
-   procedure Handle_Subn_Vx_Vy (Vx : Register_Word; Vy : Register_Word) is
-   begin
-      null;
-   end Handle_Subn_Vx_Vy;
-   procedure Handle_Shl_Vx_Vy (Vx : Register_Word; Vy : Register_Word) is
-   begin
-      null;
-   end Handle_Shl_Vx_Vy;
-   procedure Handle_Sne_Vx_Vy (Vx : Register_Word; Vy : Register_Word) is
-   begin
-      null;
-   end Handle_Sne_Vx_Vy;
-   procedure Handle_Drw_Vx_Vy_Nibble (Vx : Register_Word; Vy : Register_Word)
+
+   procedure Handle_Ld_Vx_Byte (Register_1 : General_Register_Number; B : Byte)
    is
    begin
-      null;
+      Set_General_Register (Register_1, Register_Word (B));
+   end Handle_Ld_Vx_Byte;
+
+   procedure Handle_Add_Vx_Byte
+     (Register_1 : General_Register_Number; B : Byte) is
+   begin
+      Add_General_Register (Register_1, Register_Word (B));
+   end Handle_Add_Vx_Byte;
+
+   procedure Handle_Rnd_Vx_Byte
+     (Register_1 : General_Register_Number; B : Byte) is
+   begin
+      Set_General_Register
+        (Register_1,
+         Register_Word (Random_Numbers.Get_Random_Number)
+         and Register_Word (B));
+   end Handle_Rnd_Vx_Byte;
+
+   procedure Handle_Skp_Vx (Register_1 : General_Register_Number) is
+      Key : constant Register_Word := Get_General_Register (Register_1);
+   begin
+      if Keypad.Key_Is_Pressed (Keypad.Keypad_Key (Key)) then
+         Increment_Program_Counter;
+      end if;
+   end Handle_Skp_Vx;
+
+   procedure Handle_Sknp_Vx (Register_1 : General_Register_Number) is
+      Key : constant Register_Word := Get_General_Register (Register_1);
+   begin
+      if not Keypad.Key_Is_Pressed (Keypad.Keypad_Key (Key)) then
+         Increment_Program_Counter;
+      end if;
+   end Handle_Sknp_Vx;
+
+   procedure Handle_Ld_Vx_Dt (Register_1 : General_Register_Number) is
+      Value_Delay_Timer : constant Timers.Timer := Timers.Get_Delay_Timer;
+   begin
+      Set_General_Register (Register_1, Register_Word (Value_Delay_Timer));
+   end Handle_Ld_Vx_Dt;
+
+   procedure Handle_Ld_Vx_K (Register_1 : General_Register_Number) is
+      Key : constant Keypad.Keypad_Key := Keypad.Wait_For_Keypress;
+   begin
+      Set_General_Register (Register_1, Register_Word (Key));
+   end Handle_Ld_Vx_K;
+
+   procedure Handle_Ld_Dt_Vx (Register_1 : General_Register_Number) is
+      Value : constant Register_Word := Get_General_Register (Register_1);
+   begin
+      Timers.Set_Delay_Timer (Timers.Timer (Value));
+   end Handle_Ld_Dt_Vx;
+
+   procedure Handle_Ld_St_Vx (Register_1 : General_Register_Number) is
+      Value : constant Register_Word := Get_General_Register (Register_1);
+   begin
+      Timers.Set_Sound_Timer (Timers.Timer (Value));
+   end Handle_Ld_St_Vx;
+
+   -- TODO: check if it fits in User_Address
+   procedure Handle_Add_I_Vx (Register_1 : General_Register_Number) is
+      Value_R1               : constant Register_Word :=
+        Get_General_Register (Register_1);
+      Value_Address_Register : constant Address := Get_Address_Register;
+      Final_Address          : constant Address :=
+        User_Address (Positive (Value_Address_Register) + Positive (Value_R1));
+   begin
+      Set_Address_Register (Final_Address);
+   end Handle_Add_I_Vx;
+
+   procedure Handle_Ld_F_Vx (Register_1 : General_Register_Number) is
+      Value          : constant Register_Word :=
+        Get_General_Register (Register_1);
+      Sprite_Address : constant Font_Address :=
+        Memory.Font_Address'First
+        + Address ((Value * Sprite.Hex_Sprite'Length));
+   begin
+      Set_Address_Register (Sprite_Address);
+   end Handle_Ld_F_Vx;
+
+   procedure Handle_Ld_B_Vx (Register_1 : General_Register_Number) is
+      BCD_Value              : constant Conversions.BCD :=
+        Conversions.To_BCD (Integer (Get_General_Register (Register_1)));
+      Address_Register_Value : constant User_Address := Get_Address_Register;
+   begin
+      Memory.Store (Address_Register_Value, Memory_Word (BCD_Value (1)));
+      Memory.Store (Address_Register_Value + 1, Memory_Word (BCD_Value (2)));
+      Memory.Store (Address_Register_Value + 2, Memory_Word (BCD_Value (3)));
+   end Handle_Ld_B_Vx;
+
+   -- TODO: precodition that there's enough space
+   procedure Handle_Ld_I_Vx (Register_1 : General_Register_Number) is
+      Current_Address : Address := Get_Address_Register;
+   begin
+      for Register in General_Register_Number'First .. Register_1 loop
+         Memory.Store
+           (Current_Address, Memory_Word (Get_General_Register (Register)));
+         Current_Address := Current_Address + 1;
+      end loop;
+   end Handle_Ld_I_Vx;
+
+   procedure Handle_Ld_Vx_I (Register_1 : General_Register_Number) is
+      Current_Address : Address := Get_Address_Register;
+   begin
+      for Register in General_Register_Number'First .. Register_1 loop
+         Set_General_Register
+           (Register, Register_Word (Memory.Load (Current_Address)));
+         Current_Address := Current_Address + 1;
+      end loop;
+   end Handle_Ld_Vx_I;
+
+   procedure Handle_Se_Vx_Vy
+     (Register_1 : General_Register_Number;
+      Register_2 : General_Register_Number)
+   is
+      Value_R1 : constant Register_Word := Get_General_Register (Register_1);
+      Value_R2 : constant Register_Word := Get_General_Register (Register_2);
+   begin
+      if Value_R1 = Value_R2 then
+         Increment_Program_Counter;
+      end if;
+   end Handle_Se_Vx_Vy;
+
+   procedure Handle_Ld_Vx_Vy
+     (Register_1 : General_Register_Number;
+      Register_2 : General_Register_Number)
+   is
+      Value_R2 : constant Register_Word := Get_General_Register (Register_2);
+   begin
+      Set_General_Register (Register_1, Value_R2);
+   end Handle_Ld_Vx_Vy;
+
+   procedure Handle_Or_Vx_Vy
+     (Register_1 : General_Register_Number;
+      Register_2 : General_Register_Number)
+   is
+      Value_R1 : constant Register_Word := Get_General_Register (Register_1);
+      Value_R2 : constant Register_Word := Get_General_Register (Register_2);
+      Result   : constant Register_Word := Value_R1 or Value_R2;
+   begin
+      Set_General_Register (Register_1, Result);
+   end Handle_Or_Vx_Vy;
+
+   procedure Handle_And_Vx_Vy
+     (Register_1 : General_Register_Number;
+      Register_2 : General_Register_Number)
+   is
+      Value_R1 : constant Register_Word := Get_General_Register (Register_1);
+      Value_R2 : constant Register_Word := Get_General_Register (Register_2);
+      Result   : constant Register_Word := Value_R1 and Value_R2;
+   begin
+      Set_General_Register (Register_1, Result);
+   end Handle_And_Vx_Vy;
+
+   procedure Handle_Xor_Vx_Vy
+     (Register_1 : General_Register_Number;
+      Register_2 : General_Register_Number)
+   is
+      Value_R1 : constant Register_Word := Get_General_Register (Register_1);
+      Value_R2 : constant Register_Word := Get_General_Register (Register_2);
+      Result   : constant Register_Word := Value_R1 xor Value_R2;
+   begin
+      Set_General_Register (Register_1, Result);
+   end Handle_Xor_Vx_Vy;
+
+   procedure Handle_Add_Vx_Vy
+     (Register_1 : General_Register_Number;
+      Register_2 : General_Register_Number)
+   is
+      Value_R2 : constant Register_Word := Get_General_Register (Register_2);
+   begin
+      Add_General_Register (Register_1, Value_R2);
+   end Handle_Add_Vx_Vy;
+
+   procedure Handle_Sub_Vx_Vy
+     (Register_1 : General_Register_Number;
+      Register_2 : General_Register_Number)
+   is
+      Value_R2 : constant Register_Word := Get_General_Register (Register_2);
+   begin
+      Sub_General_Register (Register_1, Value_R2);
+   end Handle_Sub_Vx_Vy;
+
+   procedure Handle_Shr_Vx (Register_1 : General_Register_Number) is
+   begin
+      Shift_Right_General_Register (Register_1);
+   end Handle_Shr_Vx;
+
+   procedure Handle_Subn_Vx_Vy
+     (Register_1 : General_Register_Number;
+      Register_2 : General_Register_Number) is
+   begin
+      SubN_General_Register (Register_1, Register_2);
+   end Handle_Subn_Vx_Vy;
+
+   procedure Handle_Shl_Vx (Register_1 : General_Register_Number) is
+   begin
+      Shift_Left_General_Register (Register_1);
+   end Handle_Shl_Vx;
+
+   procedure Handle_Sne_Vx_Vy
+     (Register_1 : General_Register_Number;
+      Register_2 : General_Register_Number)
+   is
+      Value_R1 : constant Register_Word := Get_General_Register (Register_1);
+      Value_R2 : constant Register_Word := Get_General_Register (Register_2);
+   begin
+      if Value_R1 /= Value_R2 then
+         Increment_Program_Counter;
+      end if;
+   end Handle_Sne_Vx_Vy;
+
+   procedure Handle_Drw_Vx_Vy_Nibble
+     (Register_1  : General_Register_Number;
+      Register_2  : General_Register_Number;
+      Sprite_Size : Nibble)
+   is
+      X_Pos : constant Display.X_Coordinate :=
+        Display.X_Coordinate (Get_General_Register (Register_1));
+      Y_Pos : constant Display.Y_Coordinate :=
+        Display.Y_Coordinate (Get_General_Register (Register_2));
+   begin
+      Display.Draw_Sprite
+        (Location => Get_Address_Register,
+         Size     => Positive (Sprite_Size),
+         X_Pos    => X_Pos,
+         Y_Pos    => Y_Pos);
    end Handle_Drw_Vx_Vy_Nibble;
 end Instructions;
--- src/instructions.ads
+++ src/instructions.ads
@@ -2,20 +2,23 @@ with Registers; use Registers;
 with Memory;    use Memory;
 
 package Instructions is
+   Instruction_Length : constant Integer :=
+     2; -- 2 Memory_Words per Instruction
    procedure Step;  --  does fetch, decode & execute
 private
    Opcode_Length : constant Integer := 2**16;
    type Opcode is mod Opcode_Length;
 
-   subtype NNN is Address;
+   subtype NNN is User_Address;
    type N is mod 2**4;
+   subtype Nibble is N;
    type X is mod 2**4;
    type Y is mod 2**4;
    type KK is mod 2**8;
    subtype Byte is KK;
 
    procedure Fetch (O : out Opcode);
-   procedure Decode_And_Execute
+   procedure Execute
      (O :
         Opcode);   -- Conceptionally decode and execute are two different steps, but there is no real advantage of implementing it that way imo
 
@@ -23,40 +26,67 @@ private
    procedure Handle_Cls;
    procedure Handle_Ret;
 
-   procedure Handle_Sys_Addr (Addr : Address);
-   procedure Handle_Jp_Addr (Addr : Address);
-   procedure Handle_Call_Addr (Addr : Address);
-   procedure Handle_Jp_V0_Addr (Addr : Address);
-   procedure Handle_Ld_I_Addr (Addr : Address);
+   procedure Handle_Sys_Addr (Target_Address : User_Address);
+   procedure Handle_Jp_Addr (Target_Address : User_Address);
+   procedure Handle_Call_Addr (Target_Address : User_Address);
+   procedure Handle_Jp_V0_Addr (Target_Address : User_Address);
+   procedure Handle_Ld_I_Addr (Target_Address : Address);
 
-   procedure Handle_Se_Vx_Byte (Vx : Register_Word; B : Byte);
-   procedure Handle_Sne_Vx_Byte (Vx : Register_Word; B : Byte);
-   procedure Handle_Ld_Vx_Byte (Vx : Register_Word; B : Byte);
-   procedure Handle_Add_Vx_Byte (Vx : Register_Word; B : Byte);
-   procedure Handle_Rnd_Vx_Byte (Vx : Register_Word; B : Byte);
+   procedure Handle_Se_Vx_Byte
+     (Register_1 : General_Register_Number; B : Byte);
+   procedure Handle_Sne_Vx_Byte
+     (Register_1 : General_Register_Number; B : Byte);
+   procedure Handle_Ld_Vx_Byte
+     (Register_1 : General_Register_Number; B : Byte);
+   procedure Handle_Add_Vx_Byte
+     (Register_1 : General_Register_Number; B : Byte);
+   procedure Handle_Rnd_Vx_Byte
+     (Register_1 : General_Register_Number; B : Byte);
 
-   procedure Handle_Skp_Vx (Vx : Register_Word);
-   procedure Handle_Sknp_Vx (Vx : Register_Word);
-   procedure Handle_Ld_Vx_Dt (Vx : Register_Word);
-   procedure Handle_Ld_Vx_K (Vx : Register_Word);
-   procedure Handle_Ld_Dt_Vx (Vx : Register_Word);
-   procedure Handle_Ld_St_Vx (Vx : Register_Word);
-   procedure Handle_Add_I_Vx (Vx : Register_Word);
-   procedure Handle_Ld_F_Vx (Vx : Register_Word);
-   procedure Handle_Ld_B_Vx (Vx : Register_Word);
-   procedure Handle_Ld_I_Vx (Vx : Register_Word);
-   procedure Handle_Ld_Vx_I (Vx : Register_Word);
+   procedure Handle_Ld_I_Vx (Register_1 : General_Register_Number);
+   procedure Handle_Ld_Vx_I (Register_1 : General_Register_Number);
+   procedure Handle_Skp_Vx (Register_1 : General_Register_Number);
+   procedure Handle_Sknp_Vx (Register_1 : General_Register_Number);
+   procedure Handle_Ld_Vx_Dt (Register_1 : General_Register_Number);
+   procedure Handle_Ld_Vx_K (Register_1 : General_Register_Number);
+   procedure Handle_Ld_Dt_Vx (Register_1 : General_Register_Number);
+   procedure Handle_Ld_St_Vx (Register_1 : General_Register_Number);
+   procedure Handle_Add_I_Vx (Register_1 : General_Register_Number);
+   procedure Handle_Ld_F_Vx (Register_1 : General_Register_Number);
+   procedure Handle_Ld_B_Vx (Register_1 : General_Register_Number);
+   procedure Handle_Shr_Vx (Register_1 : General_Register_Number);
+   procedure Handle_Shl_Vx (Register_1 : General_Register_Number);
 
-   procedure Handle_Se_Vx_Vy (Vx : Register_Word; Vy : Register_Word);
-   procedure Handle_Ld_Vx_Vy (Vx : Register_Word; Vy : Register_Word);
-   procedure Handle_Or_Vx_Vy (Vx : Register_Word; Vy : Register_Word);
-   procedure Handle_And_Vx_Vy (Vx : Register_Word; Vy : Register_Word);
-   procedure Handle_Xor_Vx_Vy (Vx : Register_Word; Vy : Register_Word);
-   procedure Handle_Add_Vx_Vy (Vx : Register_Word; Vy : Register_Word);
-   procedure Handle_Sub_Vx_Vy (Vx : Register_Word; Vy : Register_Word);
-   procedure Handle_Shr_Vx_Vy (Vx : Register_Word; Vy : Register_Word);
-   procedure Handle_Subn_Vx_Vy (Vx : Register_Word; Vy : Register_Word);
-   procedure Handle_Shl_Vx_Vy (Vx : Register_Word; Vy : Register_Word);
-   procedure Handle_Sne_Vx_Vy (Vx : Register_Word; Vy : Register_Word);
-   procedure Handle_Drw_Vx_Vy_Nibble (Vx : Register_Word; Vy : Register_Word);
+   procedure Handle_Se_Vx_Vy
+     (Register_1 : General_Register_Number;
+      Register_2 : General_Register_Number);
+   procedure Handle_Ld_Vx_Vy
+     (Register_1 : General_Register_Number;
+      Register_2 : General_Register_Number);
+   procedure Handle_Or_Vx_Vy
+     (Register_1 : General_Register_Number;
+      Register_2 : General_Register_Number);
+   procedure Handle_And_Vx_Vy
+     (Register_1 : General_Register_Number;
+      Register_2 : General_Register_Number);
+   procedure Handle_Xor_Vx_Vy
+     (Register_1 : General_Register_Number;
+      Register_2 : General_Register_Number);
+   procedure Handle_Add_Vx_Vy
+     (Register_1 : General_Register_Number;
+      Register_2 : General_Register_Number);
+   procedure Handle_Sub_Vx_Vy
+     (Register_1 : General_Register_Number;
+      Register_2 : General_Register_Number);
+   procedure Handle_Subn_Vx_Vy
+     (Register_1 : General_Register_Number;
+      Register_2 : General_Register_Number);
+   procedure Handle_Sne_Vx_Vy
+     (Register_1 : General_Register_Number;
+      Register_2 : General_Register_Number);
+
+   procedure Handle_Drw_Vx_Vy_Nibble
+     (Register_1  : General_Register_Number;
+      Register_2  : General_Register_Number;
+      Sprite_Size : Nibble);
 end Instructions;
