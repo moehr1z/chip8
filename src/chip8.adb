b09975c4f71974aee31e2e27c5726fb8120600b4
moehr1z <mooehr1z@gmail.com>
2025-10-23 19:03:29 +0200

    Add main loop

--- src/chip8.adb
+++ src/chip8.adb
@@ -1,4 +1,41 @@
+with Display;
+with Instructions;
+with Memory;
+with Random_Numbers;
+with Timers;
+with Ada.Text_IO;   use Ada.Text_IO;
+with Ada.Real_Time; use Ada.Real_Time;
+
 procedure Chip8 is
 begin
-   null;
+   Random_Numbers.Init;
+   Display.Init;
+   Memory.Load_Font;
+   Memory.Load_Program ("TODO");
+
+   declare
+      Micro                  : constant Float := 10.0**6;
+      Period                 : constant Time_Span :=
+        Microseconds (Integer (Micro / Float (Timers.Rate_In_Hertz)));
+      Next_Cycle             : Time := Clock;
+      Instruction_Batch_Size : constant Integer := 600;
+   begin
+      loop
+         Next_Cycle := Next_Cycle + Period;
+
+         for I in 1 .. Instruction_Batch_Size loop
+            Instructions.Step;
+         end loop;
+
+         Display.Update;
+         Timers.Update_Timers;
+
+         if Clock >= Next_Cycle then
+            Put_Line ("Deadline miss!");
+            Next_Cycle := Clock;
+         end if;
+
+         delay until Next_Cycle;
+      end loop;
+   end;
 end Chip8;
