1c02cc53559a7ec9d0b98d3b30b538bf59c13522
moehr1z <mooehr1z@gmail.com>
2025-10-26 17:50:42 +0100

    Use updated display interface

--- src/chip8.adb
+++ src/chip8.adb
@@ -1,15 +1,32 @@
 with Display;
+use Display.Display_Bounded_String;
 with Instructions;
 with Memory;
 with Random_Numbers;
 with Timers;
-with Ada.Text_IO;   use Ada.Text_IO;
-with Ada.Real_Time; use Ada.Real_Time;
+with Ada.Text_IO;      use Ada.Text_IO;
+with Ada.Real_Time;    use Ada.Real_Time;
+with Ada.Command_Line; use Ada.Command_Line;
+
+-- TODO: arguments (e.g. rom, batch size, scaling etc)
 
 procedure Chip8 is
+   Display_Init_Result   : Display.Display_Result;
+   Display_Update_Result : Display.Display_Result;
 begin
    Random_Numbers.Init;
-   Display.Init;
+
+   Display.Init (Result => Display_Init_Result);
+   if not Display_Init_Result.Success then
+      Put_Line
+        ("Could not initialize display ("
+         & Display_Init_Result.Error'Image
+         & ")");
+      Put_Line ("Error Message: " & To_String (Display_Init_Result.Message));
+      Set_Exit_Status (1);
+      return;
+   end if;
+
    Memory.Load_Font;
    Memory.Load_Program ("TODO");
 
@@ -27,7 +44,18 @@ begin
             Instructions.Step;
          end loop;
 
-         Display.Update;
+         Display.Update (Result => Display_Update_Result);
+         if not Display_Update_Result.Success then
+            Put_Line
+              ("Could not update display ("
+               & Display_Init_Result.Error'Image
+               & ")");
+            Put_Line
+              ("Error Message: " & To_String (Display_Init_Result.Message));
+            Set_Exit_Status (1);
+            return;
+         end if;
+
          Timers.Update_Timers;
 
          if Clock >= Next_Cycle then
