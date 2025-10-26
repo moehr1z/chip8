4c61b9ded558693e20ab24cc6505f3ae2504d189
moehr1z <mooehr1z@gmail.com>
2025-10-26 17:49:19 +0100

    Display: Use SDL + add error handling

--- src/display.adb
+++ src/display.adb
@@ -1,24 +1,146 @@
+with Registers;
+with SDL.Video.Windows.Makers;
+with SDL.Video.Renderers.Makers;
+with SDL.Error;
+with Ada.Strings; use Ada.Strings;
+
 package body Display is
 
-   procedure Init is
+   procedure Init (Scale : Positive := 1; Result : out Display_Result) is
    begin
-      null;
+      Display_Scale := Scale;
+
+      if not SDL.Initialise (Flags => SDL.Enable_Screen) then
+         Result :=
+           (Success => False,
+            Error   => SDL_Error,
+            Message => To_Bounded_String (SDL.Error.Get, Right));
+         return;
+      end if;
+
+      begin
+         SDL.Video.Windows.Makers.Create
+           (Win      => Window,
+            Title    => "Chip 8",
+            Position => SDL.Natural_Coordinates'(X => 10, Y => 10),
+            Size     =>
+              SDL.Positive_Sizes'
+                (SDL.Dimension (Display_Scale * Width),
+                 SDL.Dimension (Display_Scale * Height)),
+            Flags    => 0);
+         SDL.Video.Renderers.Makers.Create (Renderer, Window);
+
+         Renderer.Set_Draw_Colour ((0, 0, 0, 255));
+         Renderer.Clear;
+         Renderer.Present;
+
+         Initialized := True;
+      exception
+         when others =>
+            Result :=
+              (Success => False,
+               Error   => SDL_Error,
+               Message => To_Bounded_String (SDL.Error.Get, Right));
+            return;
+      end;
+
    end Init;
-   procedure Update is
+
+   function Was_Initialized return Boolean
+   is (Initialized);
+
+   procedure Update (Result : out Display_Result) is
    begin
-      null;
+      begin
+         for X in Display_Array'Range (1) loop
+            for Y in Display_Array'Range (2) loop
+               if Display_Array (X, Y) = True then
+                  Renderer.Set_Draw_Colour ((255, 255, 255, 255));
+               else
+                  Renderer.Set_Draw_Colour ((0, 0, 0, 255));
+               end if;
+
+               Renderer.Fill
+                 (Rectangle =>
+                    (SDL.Coordinate (Positive (X) * Display_Scale),
+                     SDL.Coordinate (Positive (Y) * Display_Scale),
+                     SDL.Natural_Dimension (Display_Scale),
+                     SDL.Natural_Dimension (Display_Scale)));
+
+            end loop;
+         end loop;
+
+         Renderer.Present;
+      exception
+         when others =>
+            Result :=
+              (Success => False,
+               Error   => SDL_Error,
+               Message => To_Bounded_String (SDL.Error.Get, Right));
+            return;
+      end;
    end Update;
+
    procedure Draw_Sprite
      (Location : Address;
-      Size     : Positive;
+      Size     : Sprite_Row_Number;
       X_Pos    : X_Coordinate;
-      Y_Pos    : Y_Coordinate) is
+      Y_Pos    : Y_Coordinate)
+   is
+      function Load_Sprite return Sprite is
+         Current_Location : Address := Location;
+         Out_Sprite       : Sprite (1 .. Size);
+      begin
+         for Row_Index in 1 .. Size loop
+            Out_Sprite (Row_Index) :=
+              Sprite_Row_Value (Load (Current_Location));
+            Current_Location := Current_Location + 1;
+         end loop;
+
+         return Out_Sprite;
+      end Load_Sprite;
+
+      Target_Sprite : constant Sprite := Load_Sprite;
+      Current_X     : X_Coordinate := X_Pos;
+      Current_Y     : Y_Coordinate := Y_Pos;
+      Collision     : Boolean := False;
    begin
-      null;
+
+      for Row_Index in Target_Sprite'Range loop
+         declare
+            Byte         : constant Sprite_Row_Value :=
+              Target_Sprite (Row_Index);
+            Sprite_Value : Boolean := False;
+         begin
+            for Bit_Index in 0 .. 7 loop
+               Sprite_Value := (Byte and (2#10000000# / 2**Bit_Index)) /= 0;
+
+               if Sprite_Value then
+                  if Display_Array (Current_X, Current_Y) then
+                     Collision := True;
+                  end if;
+
+                  Display_Array (Current_X, Current_Y) :=
+                    not Display_Array (Current_X, Current_Y);
+               end if;
+
+               Current_X := Current_X + 1;
+            end loop;
+         end;
+         Current_Y := Current_Y + 1;
+         Current_X := X_Pos;
+      end loop;
+
+      Registers.Set_VF (Collision);
    end Draw_Sprite;
+
    procedure Clear is
    begin
-      null;
+      for X in Display_Array'Range (1) loop
+         for Y in Display_Array'Range (2) loop
+            Display_Array (X, Y) := False;
+         end loop;
+      end loop;
    end Clear;
 
 end Display;
--- src/display.ads
+++ src/display.ads
@@ -1,23 +1,61 @@
-with Sprite; use Sprite;
-with Memory; use Memory;
+with Ada.Strings.Bounded;
+with Memory;  use Memory;
+with Sprites; use Sprites;
+with SDL.Video.Windows;
+with SDL.Video.Renderers;
+
+-- TODO: user definable colors
 
 package Display is
-   Width  : constant Integer := 64;
-   Height : constant Integer := 32;
+   package Display_Bounded_String is new
+     Ada.Strings.Bounded.Generic_Bounded_Length (Max => 15);
+   use Display_Bounded_String;
+
+   type Display_Error is (SDL_Error);
+
+   type Display_Result (Success : Boolean := True) is record
+      case Success is
+         when True =>
+            null;
+
+         when False =>
+            Error   : Display_Error;
+            Message : Bounded_String;
+      end case;
+   end record;
+
+   Width  : constant := 64;
+   Height : constant := 32;
 
    type X_Coordinate is mod Width;
    type Y_Coordinate is mod Height;
 
-   procedure Init;
-   procedure Update;
+   procedure Init (Scale : Positive := 1; Result : out Display_Result)
+   with
+     Pre  => not Was_Initialized,
+     Post =>
+       (Result.Success = True and then Was_Initialized)
+       or else (Result.Success = False);
+   function Was_Initialized return Boolean;
+
+   procedure Update (Result : out Display_Result)
+   with Pre => Was_Initialized;
+
    procedure Draw_Sprite
      (Location : Address;
-      Size     : Positive;
+      Size     : Sprite_Row_Number;
       X_Pos    : X_Coordinate;
-      Y_Pos    : Y_Coordinate)
-   with Pre => Size <= Max_Sprite_Height * Integer (Sprite_Row_Value'Last);
+      Y_Pos    : Y_Coordinate);
+
    procedure Clear;
 
 private
+   Display_Array : array (X_Coordinate'Range, Y_Coordinate'Range) of Boolean :=
+     [others => [others => False]];
 
+   Window   : SDL.Video.Windows.Window;
+   Renderer : SDL.Video.Renderers.Renderer;
+
+   Initialized   : Boolean := False;
+   Display_Scale : Positive := 1;
 end Display;
