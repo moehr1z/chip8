f949868bc3fd01baa39dbf62c545d5a17cde3bd7
moehr1z <mooehr1z@gmail.com>
2025-10-30 19:13:49 +0100

    Use step result

--- src/chip8.adb
+++ src/chip8.adb
@@ -1,6 +1,7 @@
 with Display;
 use Display.Display_Bounded_String;
 with Instructions;
+use Instructions.Instruction_Bounded_String;
 with Memory;
 with Random_Numbers;
 with Timers;
@@ -8,12 +9,14 @@ with Ada.Text_IO;      use Ada.Text_IO;
 with Ada.Real_Time;    use Ada.Real_Time;
 with Ada.Command_Line; use Ada.Command_Line;
 
--- TODO: arguments (e.g. rom, batch size, scaling etc)
+-- TODO: arguments (e.g. rom, batch size, scaling, failure mode etc)
 
 procedure Chip8 is
    Display_Init_Result   : Display.Display_Result;
    Display_Update_Result : Display.Display_Result;
+   Step_Result           : Instructions.Instruction_Result;
 begin
+   -- init everything
    Random_Numbers.Init;
 
    Display.Init (Result => Display_Init_Result);
@@ -30,6 +33,7 @@ begin
    Memory.Load_Font;
    Memory.Load_Program ("TODO");
 
+   -- main loop
    declare
       Micro                  : constant Float := 10.0**6;
       Period                 : constant Time_Span :=
@@ -41,7 +45,19 @@ begin
          Next_Cycle := Next_Cycle + Period;
 
          for I in 1 .. Instruction_Batch_Size loop
-            Instructions.Step;
+            Instructions.Step (Step_Result);
+
+            if Step_Result.Success = False then
+               Put_Line
+                 ("Could not execute instruction ("
+                  & Step_Result.Error'Image
+                  & ")");
+               Put_Line ("Error Message: " & To_String (Step_Result.Message));
+               Put_Line ("Opcode: " & Step_Result.Code'Image);
+
+               Set_Exit_Status (1);
+               return;
+            end if;
          end loop;
 
          Display.Update (Result => Display_Update_Result);
