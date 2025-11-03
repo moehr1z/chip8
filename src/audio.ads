727035a54ecc6c92d71edbc47ccbb621867ac273
moehr1z <mooehr1z@gmail.com>
2025-11-03 14:04:41 +0100

    Audio: Remove SDL_Error

--- src/audio.ads
+++ src/audio.ads
@@ -5,9 +5,6 @@ use Results.Result_Bounded_String;
 package Audio is
    pragma Assertion_Policy (Check);
 
-   SDL_Error : Result_Type :=
-     (Success => False, Error => To_Bounded_String ("Test"));
-
    procedure Init (Result : out Result_Type);
    function Was_Initialized return Boolean;
 
