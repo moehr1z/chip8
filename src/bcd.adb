b1b330269717e033d9848c77a2277688d3666b15
moehr1z <mooehr1z@gmail.com>
2025-11-07 14:45:47 +0100

    BCD: Remove From_BCD + Enable spark mode + make To_BCD provable

--- src/bcd.adb
+++ src/bcd.adb
@@ -1,31 +1,42 @@
-with Ada.Numerics.Elementary_Functions; use Ada.Numerics.Elementary_Functions;
+package body BCD
+  with SPARK_Mode => On
+is
+   function Get_Number_Digits (I : Natural) return Natural is
+      Tmp           : Natural := I;
+      Number_Digits : Natural := 0;
+   begin
+      if I = 0 then
+         Number_Digits := 1;
+      else
+         while Tmp /= 0 loop
+            -- Tmp decreases faster than Number_Digits increases.
+            -- Because we exit when Tmp = 0 that also means that Number_Digits
+            -- can never be greater than Tmps initial value (I), which is within Natural bounds,
+            -- so we can't get an out of bounds exception.
+            pragma
+              Loop_Invariant
+                (Tmp'Loop_Entry - Tmp
+                   >= Number_Digits - Number_Digits'Loop_Entry);
 
-package body BCD is
+            Tmp := Tmp / 10;
+            Number_Digits := Number_Digits + 1;
+         end loop;
+      end if;
+
+      return Number_Digits;
+   end Get_Number_Digits;
+
    function To_BCD (I : Natural) return BCD_Array is
-      Number_Digits : constant Natural :=
-        (if (I = 0)
-         then 0
-         else Natural (Float'Floor (Log (Float (I), 10.0))) + 1);
-      Tmp           : Integer := I;
+      Number_Digits : constant Natural := Get_Number_Digits (I);
+      Tmp           : Natural := I;
       BCD_Result    : BCD_Array (1 .. Number_Digits);
    begin
 
       for I in reverse 1 .. Number_Digits loop
-         BCD_Result (I) := Integer (Tmp rem 10);
+         BCD_Result (I) := Digit (Tmp rem 10);
          Tmp := Tmp / 10;
       end loop;
 
       return BCD_Result;
    end To_BCD;
-
-   function From_BCD (B : BCD_Array) return Natural is
-      Sum : Natural := 0;
-      L   : constant Natural := B'Length;
-   begin
-      for J in B'Range loop
-         Sum := Sum + B (J) * 10**(L - J);
-      end loop;
-
-      return Sum;
-   end From_BCD;
 end BCD;
--- src/bcd.ads
+++ src/bcd.ads
@@ -1,8 +1,14 @@
-package BCD is
-   type BCD_Array is array (Positive range <>) of Integer range 0 .. 9;
+package BCD
+  with SPARK_Mode => On
+is
+   type Digit is range 0 .. 9;
+   -- For this purpose a constrained array with 3 elements would suffice, but I wanted to make
+   -- it unconstrained as an exercise (and maybe reusability in the future)
+   -- TODO: add functional postcondition
+   type BCD_Array is array (Positive range <>) of Digit;
 
-   function From_BCD (B : BCD_Array) return Natural;
+   function To_BCD (I : Natural) return BCD_Array;
 
-   function To_BCD (I : Natural) return BCD_Array
-   with Post => From_BCD (To_BCD'Result) = I;
+private
+   function Get_Number_Digits (I : Natural) return Natural;
 end BCD;
