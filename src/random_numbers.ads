07157678e9ca0470e84ee131e70f7ed7a429760f
moehr1z <mooehr1z@gmail.com>
2025-11-09 19:09:16 +0100

    Random_Numbers: Remove initialised state info

--- src/random_numbers.ads
+++ src/random_numbers.ads
@@ -2,17 +2,11 @@ with Ada.Numerics.Discrete_Random;
 
 package Random_Numbers is
    type Random_Number is mod 2**8;
-   procedure Init
-   with Post => Was_Initialized;
-   function Was_Initialized return Boolean;
-
-   function Get_Random_Number return Random_Number
-   with Pre => Was_Initialized;
+   procedure Init;
+   function Get_Random_Number return Random_Number;
 
 private
    package Rand_Byte is new Ada.Numerics.Discrete_Random (Random_Number);
    use Rand_Byte;
    Random_Generator : Generator;
-
-   Is_Initialized : Boolean := False;
 end Random_Numbers;
